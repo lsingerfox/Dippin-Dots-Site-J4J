1c64bb6ab6a7572ad8e5053af0e53cdf8577843a
lsingerfox <lynell.r.singer@gmail.com>
2021-05-31 20:40:16 -0700

    Renamed images and added images to header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
+import Crazy from '../../static/images/crazy-dippin-dots.jpg';
+import Cups from '../../static/images/dippin-dots-cups.jpg';
+import Logo from '../../static/images/dippin-dots-logo.jpg';
+import Spoon from '../../static/images/dippin-dots-spoon.jpg';
+import Dotted from '../../static/images/dotted-background.png';
+
 export default class Header extends Component {
     render() {
         return (
             <div>
-                <h1>Rotating Images Go Here</h1>
+                <img className="dd_logo" src={Logo} alt="Dippin' Dots Logo" />
+                <img className="dd_spoon" src={Spoon} alt="Dippin' Dots in a Spoon" />
+                <img className="dd_dotted-background" src={Dotted} alt="Dotted Dippin' Dots Background" />
+                <img className="dd_cups" src={Cups} alt="Dippin' Dots in Cups" />
+                <img className="dd_crazy" src={Crazy} alt="Crazy Dippin' Dots" />
             </div>
         )
     }
