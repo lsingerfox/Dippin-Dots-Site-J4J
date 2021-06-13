86271591a71d46a383bb0398fadf33750c98770a
lsingerfox <lynell.r.singer@gmail.com>
2021-06-12 22:39:23 -0700

    Created header-nav folder, moved header and navbar to new folder, and started creating file for carousel array.

---
+++ src/components/header-nav/carousel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PhotoCarousel extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
